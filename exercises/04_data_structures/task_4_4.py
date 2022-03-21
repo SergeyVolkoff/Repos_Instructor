c83274fd8c2a78aab7a9b8c6f8c174ea0e06eeac
SergeyVolkoff <vol7745@yandex.ru>
2022-03-21 16:48:18 +0300

    21-03

--- exercises/04_data_structures/task_4_4.py
+++ exercises/04_data_structures/task_4_4.py
@@ -24,3 +24,10 @@
 """
 
 vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+n=sorted(vlans)
+n.remove(10)
+n.remove(10)
+n.remove(30)
+result=n
+print(result)
+
--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,9 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+a=command1.split()[-1]
+print(a)
+b=command2.split()[-1]
+print(b)
+c=a+b
+print(c)
